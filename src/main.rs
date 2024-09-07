835dd565dfde936c19af41615334953470002193
Yassen Higazi <yassen.higazi@rasid.com.sa>
2024-09-07 20:52:56 +0300

    Add HTTP response to incoming connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -6,8 +7,12 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+
+                let reply = String::from("HTTP/1.1 200 OK\r\n\r\n");
+
+                _stream.write_all(reply.as_bytes()).expect("Failed to send Response to client");
             }
             Err(e) => {
                 println!("error: {}", e);
